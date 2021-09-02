03fcc7790d14f551f71c5c68285f90af46f1c836
Toshio <ctoshioyoshikawa@gmail.com>
2021-09-02 15:49:35 -0500

    to show the list of persons in homepage

--- aplicaciones/principal/views.py
+++ aplicaciones/principal/views.py
@@ -3,5 +3,9 @@ from .models import Persona
 
 # Create your views here.
 def inicio(request):
-    return render(request, 'index.html')
-    
+    personas = Persona.objects.all()  #select * from Persona
+    contexto = {
+        'personas':personas
+    }
+    return render(request, 'index.html',contexto)
+
